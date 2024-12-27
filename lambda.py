7bf5f9303c4149b83b74193f1b6ec4b35a7bf0eb
Nikolai40Kuznetsov <nikolaikuznetsov@tutamail.com>
2024-12-27 13:25:47 +0300

    ss

---
+++ lambda.py
@@ -0,0 +1,12 @@
+# Yusupov
+
+# Lambda-function
+func = lambda x: pow(pow(pow(pow(pow(x,x),x),x),x),x)
+
+
+# Main function
+def main():
+    print(func(5))
+
+if __name__ == "__main__":
+    main()
