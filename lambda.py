7dced84252c5d1a865223ddb5abdd92bdd8aa8d4
Nikolai40Kuznetsov <nikolaikuznetsov@tutamail.com>
2024-12-30 11:10:23 +0300

    dd

--- lambda.py
+++ lambda.py
@@ -1,12 +1,16 @@
 # Yusupov
 
+import sys
+
+
+sys.set_int_max_str_digits(666666)
 # Lambda-function
-func = lambda x: pow(pow(pow(pow(pow(x,x),x),x),x),x)
+func = lambda x: pow(pow(pow(pow(x,x),x),x),x)
 
 
 # Main function
 def main():
-    print(func(5))
+    print(func(100))
 
 if __name__ == "__main__":
     main()
